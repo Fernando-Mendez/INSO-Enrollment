6a5c9295eca42ced6907da15b8383008deaed89c
Fernando.Mendez <fernando.mendez@upr.edu>
2021-04-19 22:34:23 -0400

    Changed case

--- js/index.js
+++ js/index.js
@@ -50,8 +50,8 @@ window.addEventListener("load", async ()=>{
                     <td>${index+1}</td>
                     <td>${data.nameid}</td>
                     <td>${data.description}</td>
-                    <td>${data.Prereq.join(", ")}</td>
-                    <td>${data.Coreq.join(", ")}</td>
+                    <td>${data.prereq.join(", ")}</td>
+                    <td>${data.coreq.join(", ")}</td>
                     <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="green">
                         <path
                         d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
