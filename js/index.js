70ceae1519fb1ae151d65a4ab8c571d1acd6d25e
Fernando.Mendez <fernando.mendez@upr.edu>
2021-04-23 14:47:41 -0400

    Started work on curr side menu class view

--- js/index.js
+++ js/index.js
@@ -9,9 +9,11 @@ currButton.addEventListener("click",async ()=>{
 
 })
 
+let globalCourses;
+
 let tbody = document.querySelector("#enrollment-tbody");
 window.addEventListener("load", async ()=>{
-    let response = await fetch("/search", {
+    let response = await fetch("/courses", {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         headers: {
             'Content-Type': 'application/json'
@@ -32,12 +34,9 @@ window.addEventListener("load", async ()=>{
             return data[key];
         });
 
-        console.log(result);
-
         let myHTML = "";
-        
+        globalCourses = data;
         result.forEach((data, index)=>{
-            console.log("object:",data);
             myHTML += `<tr>
                     <td>${data.demand}</td>
                     <td>${data.nameid}</td>
@@ -51,6 +50,7 @@ window.addEventListener("load", async ()=>{
                 </tr>`;
         })
         tbody.innerHTML = myHTML;
+        await getCurriculum();
 })
 
 
@@ -63,8 +63,10 @@ closeButton.addEventListener("click",async ()=>{
 
 
 let cbody = document.querySelector("#curriculum-tbody");
-window.addEventListener("load", async ()=>{
-    let response = await fetch("/search", {
+const getCurriculum = async ()=>{
+
+
+    let response = await fetch("/curriculum", {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         headers: {
             'Content-Type': 'application/json'
@@ -75,31 +77,35 @@ window.addEventListener("load", async ()=>{
         // body: JSON.stringify(data) // body data type must match "Content-Type" header
         })
         
-        let data = await response.json();
-        var result = Object.keys(data).map(function (key) {
-            // Using Number() to convert key to number type
-            // Using obj[key] to retrieve key value
-            data[key]['nameid'] = key; 
-            return data[key];
-        });
+        let curriculums = await response.json();
 
-        console.log(result);
+        let ciicCur = curriculums["CIIC"]; 
 
         let myHTML = "";
         
-        result.forEach((data, index)=>{
-            console.log("object:",data);
-            myHTML += `<tr>
-                    <td>${data.demand}</td>
-                    <td>${data.nameid}</td>
-                    <td>${data.description}</td>
-                    <td>${data.prereq.join(", ")}</td>
-                    <td>${data.coreq.join(", ")}</td>
-                    <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="green">
-                        <path
-                        d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
-                    </svg></td>
+        ciicCur.courses.forEach((course, index)=>{
+            console.log("object:", course);
+            if (course.coursename.length == 8){
+                if(globalCourses[course.coursename]){
+                    myHTML += `<tr>
+                    <td>${globalCourses[course.coursename].semester}</td>
+                    <td>${globalCourses[course.coursename].nameid}</td>
+                    <td>${globalCourses[course.coursename].description}</td>
+                    <td>${globalCourses[course.coursename].prereq.join(", ")}</td>
+                    <td>${globalCourses[course.coursename].coreq.join(", ")}</td>
                 </tr>`;
+                }
+            }
+            else {
+                `<tr>
+                        <td> </td>
+                        <td>${course.coursename.nameid}</td>
+                        <td> Course Elective </td>
+                        <td> </td>
+                        <td> </td>
+                    </tr>`
+            }
         })
-        tbody.innerHTML = myHTML;
-})
+        cbody.innerHTML = myHTML;
+
+}
