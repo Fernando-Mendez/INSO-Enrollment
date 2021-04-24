2f2715c4f2c5f38004eb7a9c89ca365a07c51dd2
Fernando.Mendez <fernando.mendez@upr.edu>
2021-04-24 19:25:45 -0400

    Added new column to the table

--- js/index.js
+++ js/index.js
@@ -11,6 +11,11 @@ currButton.addEventListener("click",async ()=>{
 function transformToTableRows(data) {
     let rows = "";
     data.forEach((item)=>{
+        /* if (item.demand. == dropdownTerm){
+
+        }
+        
+        */ 
         rows += `<tr>
                 <td>${item.demand}</td>
                 <td>${item.nameid}</td>
@@ -20,7 +25,9 @@ function transformToTableRows(data) {
                 <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="green">
                     <path
                     d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
-                </svg></td>
+                </svg>
+                </td>
+                <td> <input type="checkbox"> </td>
             </tr>`;
     })
     return rows;
@@ -82,9 +89,7 @@ const getCurriculum = async ()=>{
         })
         
         let curriculums = await response.json();
-
         let ciicCur = curriculums["CIIC"]; 
-
         let myHTML = "";
         
         ciicCur.courses.forEach((course, index)=>{
